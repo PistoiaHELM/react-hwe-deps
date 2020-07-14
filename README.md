dc6a38012f2fdc65fd45a9f6ee89ede92b0e9157
Joshua Ren <joshuaren2018@gmail.com>
2020-07-14 18:41:14 -0400

    react deps module upload

---
+++ README.md
@@ -0,0 +1,10 @@
+ src
+    Pistoia HELM
+    Version 1.1.2.2019-01-17
+
+    JSDraw.Lite 
+    Version 6.0.0.2019-01-17
+        - edited version
+
+    pako 
+    Version 1.0.1
---
+++ index.js
@@ -0,0 +1,23 @@
+/**  
+ *  @module hwe-react-deps
+ *  return a promise object that resolves when the scripts load into the window
+ *  updating packages can happen here directly now
+ *  update project using 'npm update hwe-react-deps'
+ */
+module.exports = () => {
+  return new Promise((resolve, reject) => {
+    var postscribe = require('postscribe');
+    postscribe(document.body, '<script language="javascript" src="http://ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojo/dojo.js"></script>', {
+      done: () => {
+        require("./src/pako/pako.js");
+        require("./src/JSDraw/Scilligence.JSDraw2.Lite-uncompressed.js"); // line 26950 breaks app, its commented out here
+        require("./src/JSDraw/Pistoia.HELM.js");
+        require("./src/JSDraw/Scilligence.JSDraw2.Resources.js");
+        resolve('success');
+      },
+      error: (error) => {
+        reject(error);
+      }
+    });
+  });
+}
