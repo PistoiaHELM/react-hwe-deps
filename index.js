cdba2684ee73eb9f18010284e71dceb29d3cabfe
Joshua Ren <joshuaren2018@gmail.com>
2020-07-30 13:40:10 -0400

    removed postscribe dependency

--- index.js
+++ index.js
@@ -6,18 +6,15 @@
  */
 module.exports = () => {
   return new Promise((resolve, reject) => {
-    var postscribe = require('postscribe');
-    require('./src/pako/pako.js');
-    postscribe(document.body, '<script language="javascript" src="http://ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojo/dojo.js"></script>', {
-      done: () => {
-        require('./src/JSDraw/Scilligence.JSDraw2.Lite-uncompressed.js'); // line 26950 breaks app, its commented out here
-        require('./src/JSDraw/Pistoia.HELM.js');
-        require('./src/JSDraw/Scilligence.JSDraw2.Resources.js');
-        resolve('success');
-      },
-      error: (error) => {
-        reject(error);
-      }
-    });
+    var s = document.createElement("script");
+    s.setAttribute("src","http://ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojo/dojo.js");
+    s.onload = () => {
+      require('./src/pako/pako.js');
+      require('./src/JSDraw/Scilligence.JSDraw2.Lite-uncompressed.js'); // line 26950 breaks app, its commented out here
+      require('./src/JSDraw/Pistoia.HELM.js');
+      require('./src/JSDraw/Scilligence.JSDraw2.Resources.js');
+      resolve('success');
+    }
+    document.head.appendChild(s);
   });
 }
