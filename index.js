6daece83de437936bfa0c26ad5cb217dd6244c45
Joshua Ren <joshuaren2018@gmail.com>
2020-07-30 13:26:07 -0400

    load pako first

--- index.js
+++ index.js
@@ -7,9 +7,9 @@
 module.exports = () => {
   return new Promise((resolve, reject) => {
     var postscribe = require('postscribe');
+    require('./src/pako/pako.js');
     postscribe(document.body, '<script language="javascript" src="http://ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojo/dojo.js"></script>', {
       done: () => {
-        require('./src/pako/pako.js');
         require('./src/JSDraw/Scilligence.JSDraw2.Lite-uncompressed.js'); // line 26950 breaks app, its commented out here
         require('./src/JSDraw/Pistoia.HELM.js');
         require('./src/JSDraw/Scilligence.JSDraw2.Resources.js');
