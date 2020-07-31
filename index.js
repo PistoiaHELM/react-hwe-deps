17bb245dd87fe772e94e72b2afff6ccb0d44eed0
Joshua Ren <joshuaren2018@gmail.com>
2020-07-31 13:50:04 -0400

    fixed dojox bug

--- index.js
+++ index.js
@@ -6,15 +6,17 @@
  */
 module.exports = () => {
   return new Promise((resolve, reject) => {
-    var s = document.createElement("script");
-    s.setAttribute("src","http://ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojo/dojo.js");
-    s.onload = () => {
-      require('./src/pako/pako.js');
-      require('./src/JSDraw/Scilligence.JSDraw2.Lite-uncompressed.js'); // line 26950 breaks app, its commented out here
-      require('./src/JSDraw/Pistoia.HELM.js');
-      require('./src/JSDraw/Scilligence.JSDraw2.Resources.js');
-      resolve('success');
-    }
-    document.head.appendChild(s);
+      var s = document.createElement("script");
+      s.setAttribute("src","http://ajax.googleapis.com/ajax/libs/dojo/1.10.4/dojo/dojo.js");
+      s.onload = () => {
+        require('./src/pako/pako.js');
+        require('./src/JSDraw/Scilligence.JSDraw2.Lite-uncompressed.js'); // line 26950 breaks app, its commented out here
+        require('./src/JSDraw/Pistoia.HELM.js');
+        require('./src/JSDraw/Scilligence.JSDraw2.Resources.js');
+        window.addEventListener('load', () => {
+          resolve('success');
+        });
+      }
+      document.head.appendChild(s);
   });
 }
